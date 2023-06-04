2c2cda36b2adf32529762ee3f6e9cce1473a187a
antoni-martinek <antoni.martinek@zhr.pl>
2023-06-05 00:59:42 +0200

    Dodanie biblioteki SFML i utworzenie okna gry

--- project_game/main.cpp
+++ project_game/main.cpp
@@ -1,9 +1,29 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <cmath>
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world everyone!" << endl;
-    return 0;
+    float window_x = 650.0;
+    float window_y = 800.0;
+    sf::RenderWindow window(sf::VideoMode(window_x, window_y), "SPIKY BIRD");
+    window.setFramerateLimit(60);
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        window.display();
+    }
 }
