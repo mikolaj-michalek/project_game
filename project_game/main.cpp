805654788e60390761270b62d496f162328970a6
mikolaj-michalek <mikolaj.michalek@onet.eu>
2023-06-05 01:21:59 +0200

    Dodanie tekstury 'bird', utworzenie animacji

--- project_game/main.cpp
+++ project_game/main.cpp
@@ -12,15 +12,23 @@ int main()
 {
     float window_x = 650.0;
     float window_y = 800.0;
-    sf::RenderWindow window(sf::VideoMode(window_x, window_y), "SPIKY BIRD");
+    sf::RenderWindow window(sf::VideoMode(window_x, window_y), "Spiky Bird");
     window.setFramerateLimit(60);
 
     sf::Texture texture_background;
     if(!texture_background.loadFromFile("background.png")) { return 1; }
-    texture_background.setRepeated(true);
+
+    sf::Texture texture_bird;
+    if(!texture_bird.loadFromFile("bird_fly.png")) { return 1; }
 
     sf::Sprite background;
     background.setTexture(texture_background);
+
+    sf::Sprite bird;
+    bird.setTexture(texture_bird);
+    bird.setPosition(50, 50);
+
+    float birdSpeed = 3.0f;
 
 
     while (window.isOpen()) {
@@ -30,7 +38,25 @@ int main()
                 window.close();
         }
 
+        sf::Vector2f movement(0.0f, 0.0f);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            movement.y -= birdSpeed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            movement.y += birdSpeed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            movement.x -= birdSpeed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            movement.x += birdSpeed;
+
+        // Aktualizacja pozycji ptaka
+        sf::Vector2f newPosition = bird.getPosition() + movement;
+
+        bird.setPosition(newPosition);
+
         window.draw(background);
+
+        window.draw(bird);
 
         window.display();
     }
